7e5426ede853e9e0256950e8da5535d591f53350
RonnySk <ronny_sk@hotmail.com>
2024-01-13 13:05:03 +0100

    create new folders and add logo

---
+++ src/app/components/GroverLogo/page.tsx
@@ -0,0 +1,14 @@
+import Image from "../../../../node_modules/next/image";
+
+const GroverLogo = () => {
+  return (
+    <Image
+      src="/Grover_Logo_TorchRed_RGB_large.png"
+      width={150}
+      height={150}
+      alt="grover logo"
+    />
+  );
+};
+
+export default GroverLogo;
