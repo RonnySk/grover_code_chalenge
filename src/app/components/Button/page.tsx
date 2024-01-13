9416ccecfc6333b3ae52a3a7c81a8e489f5cbdbd
RonnySk <ronny_sk@hotmail.com>
2024-01-13 13:20:57 +0100

    button update

--- src/app/components/Button/page.tsx
+++ src/app/components/Button/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Button = () => {
-  return <div>Button</div>;
+  return (
+    <>
+      <button className="border-2 border-white w-36 p-2 hover:bg-gray-500">
+        Extend rental
+      </button>
+    </>
+  );
 };
 
 export default Button;
--- src/app/components/SubscriptionCard/page.tsx
+++ src/app/components/SubscriptionCard/page.tsx
@@ -3,7 +3,7 @@ import Button from "../Button/page";
 const SubscriptionCard = (product) => {
   return (
     <>
-      <div className="flex flex-col border-2 border-white w-3/4 p-16 ">
+      <div className="flex flex-col gap-2 border-2 border-white w-3/4 p-16 ">
         <div>product.image</div>
         <div>product.title</div>
         {/* here logic to date activt */}
